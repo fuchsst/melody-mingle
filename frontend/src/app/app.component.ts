88c9d5f61bda86a96c0a2067445414702dac9c0c
Demirkol, Goerkem <gdemirkol315@gmail.com>
2024-01-11 22:34:48 +0100

    start screen component added

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { StartScreenComponent } from './components/start-screen/start-screen.component'; // Adjust the path as necessary
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,StartScreenComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ frontend/src/app/components/start-screen/start-screen.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'start-screen',
+  standalone: true,
+  imports: [],
+  templateUrl: './start-screen.component.html',
+  styleUrl: './start-screen.component.scss'
+})
+export class StartScreenComponent {
+
+}
