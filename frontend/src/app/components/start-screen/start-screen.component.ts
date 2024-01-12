07a90bc93f23edf5bb71cee6669eef0c42075b8d
Demirkol, Goerkem <gdemirkol315@gmail.com>
2024-01-12 21:47:13 +0100

    angular material added to project, example button added

--- frontend/src/app/components/start-screen/start-screen.component.ts
+++ frontend/src/app/components/start-screen/start-screen.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import {MatButtonModule} from "@angular/material/button";
+import {MatToolbarModule} from "@angular/material/toolbar";
 @Component({
   selector: 'start-screen',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule,MatToolbarModule],
   templateUrl: './start-screen.component.html',
   styleUrl: './start-screen.component.scss'
 })
